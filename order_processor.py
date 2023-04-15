f0c289419c0eba4ec1d66fdee7d60a4df0ec6d22
Juan <arenas.juan.f@gmail.com>
2023-04-15 12:34:14 -0700

    Fixed issues with tag assignments; implemented FFD algorithm for sorting items into child orders; fixed issue with STK presets overwriting any order containing an STK; added threads for post requests to create parent and child orders simultaneously

--- order_processor.py
+++ order_processor.py
@@ -6,10 +6,11 @@ def lambda_handler(event, context):
     orders = functions.extract_data_from_resource_url(event)
     print(f"Number of orders: {len(orders)}")
 
-    with ThreadPoolExecutor(max_workers=8) as executor:
-        futures = [executor.submit(functions.processor, order) for order in orders]
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        futures = [executor.submit(process_order, order, get_mlp_data(order['orderNumber']), parent_has_gnome=False) for order in orders]
         for future in as_completed(futures):
             future.result()
+
     if len(functions.failed) == 0:
         print("All orders processed successfully!")
     else:
